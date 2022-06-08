6dc3a09275d277efc359eb197633c54e5ffef81d
Artem Ermilov <ermilo-artjom@rambler.ru>
2022-06-08 18:10:30 +0300

    Задача по нахождению индекса массива Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {11,22,33,44,55,16,17,18,44};
+
+int n = array.Length; //array.Length;
+int find = 44;
+
+int index = 0;
+
+while (index<n)
+{
+    if (array[index]==find )
+    {
+        Console.WriteLine(index);
+        break;
+
+    }
+    
+    index++;
+}
